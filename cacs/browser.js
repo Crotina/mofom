9dbb1e1c6e5005af5fc78d8d418c481a9c1e1ff8
KawaiiYooo <133883796+Crotina@users.noreply.github.com>
2026-02-09 18:19:29 -0800

    Delete cacs/browser.js

--- cacs/browser.js
+++
@@ -1,8 +0,0 @@
-const main_btn = document.getElementById("controlBtn");
-const ipt = document.getElementById("v8");
-const iframe_ = document.getElementById("ifr");
-
-function redirect (){
-    console.log(main_btn + "and" + ipt.value);
- iframe_.src = ipt.value;
-}
